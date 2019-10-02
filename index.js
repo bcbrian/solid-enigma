2b698c3100a469fad00b1a138674d56c15a7de6f
Brian Bartholomew <me@bcbrian.com>
2019-10-02 10:40:34 -0500

    mul

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ function sum(a, b) {
 function sub(a, b) {
   return a - b;
 }
+
+function mul(a, b) {
+  return a * b;
+}
+
 module.exports = {
   sum,
-  sub
+  sub,
+  mul
 };
--- index.test.js
+++ index.test.js
@@ -6,5 +6,14 @@ test('adds 1 + 2 to equal 3', () => {
 
 
 test('subs 1 - 2 to equal -1', () => {
-    expect(calc.sub(1, 2)).toBe(-1);
-  });
+  expect(calc.sub(1, 2)).toBe(-1);
+});
+
+test('muls 1 * 2 to equal 2', () => {
+  expect(calc.mul(1, 2)).toBe(2);
+});
+
+test('muls 1 * 3 to equal 3', () => {
+  expect(calc.mul(1, 3)).toBe(3);
+});
+
