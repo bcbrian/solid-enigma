c20a05bd852bd01b467e937354bed46ba36d4fa6
Brian Bartholomew <me@bcbrian.com>
2019-10-02 10:31:05 -0500

    sub

--- index.test.js
+++ index.test.js
@@ -3,3 +3,8 @@ const calc = require('./');
 test('adds 1 + 2 to equal 3', () => {
   expect(calc.sum(1, 2)).toBe(3);
 });
+
+
+test('subs 1 - 2 to equal -1', () => {
+    expect(calc.sub(1, 2)).toBe(-1);
+  });
